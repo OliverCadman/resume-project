68359ad61e5682e8cd68abe375f8445262993b24
OliverCadman <o.cadman@live.co.uk>
2021-07-03 19:20:28 +0100

    Implemented rendering of Github repo data

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,3 +1,7 @@
+$(document).ready(function() {
+  userInformationHTML();
+})
+
 function userInformationHTML(user) {
   return `
     <h2>${user.name}
@@ -17,6 +21,25 @@ function userInformationHTML(user) {
     </div>`;
 }
 
+function repoInformationHTML(repos) {
+  if (repos.length == 0) {
+    return `<div class="clearfix repo-list">No repos!</div>`;
+  }
+  var listItemsHTML = repos.map((repo) => {
+    return `<li>
+              <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+              </li>`;
+  });
+  return `<div class="clearfix repo-list">
+            <p>
+              <strong>Repo List:</strong>
+            </p>
+            <ul>
+            ${listItemsHTML.join("\n")}
+            </ul>
+            </div>`;
+}
+
 function fetchGithubInformation(event) {
   var username = $("#gh-username").val();
   if (!username) {
@@ -30,10 +53,15 @@ function fetchGithubInformation(event) {
         </div>`
   );
 
-  $.when($.getJSON(`https://api.github.com/users/${username}`)).then(
-    function (response) {
-      var userData = response;
+  $.when(
+    $.getJSON(`https://api.github.com/users/${username}`),
+    $.getJSON(`https://api.github.com/users/${username}/repos`)
+  ).then(
+    function (firstResponse, secondResponse) {
+      var userData = firstResponse[0];
+      var repoData = secondResponse[0]
       $("#gh-user-data").html(userInformationHTML(userData));
+      $("#gh-repo-data").html(repoInformationHTML(repoData))
     },
     function (errorResponse) {
       if (errorResponse.status === 404) {
